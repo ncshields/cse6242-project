dc8ad11b2f47375d892642421b7335c089cf997f
Amir Ziai <arziai@gmail.com>
2018-10-17 09:37:05 -0700

    adding type hints

--- datasets.py
+++ datasets.py
@@ -1,7 +1,8 @@
 import csv
+from typing import List
 
 DATASETS_PATH = 'resources/datasets/'
 
 
-def get_bbc():
+def get_bbc() -> List[str]:
     return [line[1] for i, line in enumerate(csv.reader(open(f"{DATASETS_PATH}bbc-text.csv"), delimiter=',')) if i > 0]
--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,16 @@
+from typing import Tuple, List
+
+from scipy.sparse.csr import csr_matrix
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
 class NLPProcessor:
     def __init__(self,
-                 vectorizer='tf-idf',
-                 ngram_range=(1, 1),
-                 stop_words=None,
-                 max_features=None
-                 ):
+                 vectorizer: str='tf-idf',
+                 ngram_range: Tuple[int]=(1, 1),
+                 stop_words: str=None,
+                 max_features: int=None
+                 ) -> None:
         """
         Initialize processor.
         :param vectorizer: choice of vectorizer
@@ -24,7 +27,7 @@ class NLPProcessor:
 
         self.vec = vectorizers[vectorizer](ngram_range=ngram_range, stop_words=stop_words, max_features=max_features)
 
-    def fit_transform(self, corpus):
+    def fit_transform(self, corpus: List[str]) -> csr_matrix:
         """
         Take a list of strings (docs) and returns a vector representation.
         :param corpus: list of docs
