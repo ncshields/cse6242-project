9b0c4b1aa2b6c600f7185b5cd57890bd5059f5d9
cpu-meltdown <n.baalbaki91@gmail.com>
2018-10-25 18:49:08 -0400

    added flasgger and documentation

--- examples/flask-example/app.py
+++ examples/flask-example/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
+Swagger(app)
 
 class InvalidUsage(Exception):
     status_code = 400
@@ -24,18 +24,68 @@ def handle_invalid_usage(error):
     response.status_code = error.status_code
     return response
 
-@app.route('/merge', methods=['POST', 'GET'])
+@app.route('/api/merge', methods=['POST'])
 def merge_topics():
-    topic1 = request.args.get('topic1')
-    topic2 = request.args.get('topic2')
-    if topic1 == None or topic2 == None:
-    	raise InvalidUsage('provide both topic1 and topic2')
-    return "merging " + topic1 + " " + topic2
+	"""
+    This is an api to merge two topics
+    Call this api passing topic1 and topic2 as query params
+    ---
+    tags:
+      - Merge Topics API
+    parameters:
+      - name: topic1
+        in: query
+        type: string
+        required: true
+        description: The first topic to merge
+      - name: topic2
+        in: query
+        type: string
+        required: true
+        description: The second topic to merge
+    responses:
+      400:
+        description: Validation issue. One or both topics were not provided
+      500:
+		description: Internal server error
+      200:
+        description: The two topics were merged
+    """
+	topic1 = request.args.get('topic1')
+	topic2 = request.args.get('topic2')
+	if topic1 == None or topic2 == None:
+		raise InvalidUsage('provide both topic1 and topic2')
+	return "merging " + topic1 + " " + topic2
 
-@app.route('/split', methods=['POST', 'GET'])
+@app.route('/split', methods=['POST'])
 def split_topics():
-    topic1 = request.args.get('topic1')
-    topic2 = request.args.get('topic2')
-    if topic1 == None or topic2 == None:
-    	raise InvalidUsage('provide both topic1 and topic2')
-    return "spliting " + topic1 + " " + topic2
+	"""
+    This is an api to split two topics
+    Call this api passing topic1 and topic2 as query params
+    ---
+    tags:
+      - Split Topics API
+    parameters:
+      - name: topic1
+        in: query
+        type: string
+        required: true
+        description: The first topic to split
+      - name: topic2
+        in: query
+        type: string
+        required: true
+        description: The second topic to split
+    responses:
+      400:
+        description: Validation issue. One or both topics were not provided
+      500:
+		description: Internal server error
+      200:
+        description: The two topics were splitted
+    """
+	topic1 = request.args.get('topic1')
+	topic2 = request.args.get('topic2')
+	if topic1 == None or topic2 == None:
+		raise InvalidUsage('provide both topic1 and topic2')
+	return "spliting " + topic1 + " " + topic2
